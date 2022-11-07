27268860c310cd191984835663ede90387a54836
Rokiatou <diallorokiatou1@gmail.com>
2022-11-07 16:18:04 +0100

    wrap edges

--- src/com/lacombe/model/Point.java
+++ src/com/lacombe/model/Point.java
@@ -52,19 +52,4 @@ public class Point {
         return Objects.hash(x, y);
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
 }
