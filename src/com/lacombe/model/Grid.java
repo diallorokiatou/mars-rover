6f849831ad71aa716343482c28610311d29433d4
Rokiatou <diallorokiatou1@gmail.com>
2022-11-09 11:30:22 +0100

    refratoring

--- src/com/lacombe/model/Grid.java
+++ src/com/lacombe/model/Grid.java
@@ -1,34 +1,30 @@
 package src.com.lacombe.model;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Grid {
     public Set<GridCell> cells;
 
     public Grid(int size, Point ...obstaclePoints) {
         this.cells = new HashSet<>();
-        init(size);
-        for(Point point : obstaclePoints){
-            setObstacleAt(point);
-        }
-    }
-
-    private void init(int size) {
+        List<Point> obtaclesList = toList(obstaclePoints);
         for(int i = 1; i <= size; i++){
             for(int j = 1; j <= size; j++){
                 Point point = new Point(i, j, size);
-                cells.add(new GridCell(point));
+                GridCell gridCell = new GridCell(point);
+                if(obtaclesList.contains(point)) {
+                    gridCell.setObstacle();
+                }
+                cells.add(gridCell);
             }
         }
     }
 
-    public void setObstacleAt(Point point){
-        cells = cells.stream().peek(cell -> {
-                    if(cell.point.equals(point)) cell.setObstacle();
-                    }).collect(Collectors.toSet());
+    private List<Point> toList(Point ...obstaclePoints) {
+        if(obstaclePoints == null) return Collections.emptyList();
+        return  Arrays.asList(obstaclePoints);
     }
+
     public boolean hasObstacleAt(Point point){
         return cells.stream().anyMatch(cell -> cell.point.equals(point) && cell.hasObstacle);
     }
